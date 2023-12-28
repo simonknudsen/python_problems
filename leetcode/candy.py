c1fec504cfe7d395c1eca3b74a3a7c5207b23011
Randall Knudsen <simonjknudsen@gmail.com>
2023-12-28 14:40:20 +1300

    WIP candy

--- leetcode/candy.py
+++ leetcode/candy.py
@@ -16,23 +16,23 @@ class Solution:
         while not all(candies) :
             prev_rating = min_rating
             next_rating = min_rating
-            prev_candie = 1
-            next_candie = 1
+            prev_candy = None
+            next_candy = None
             for i in range(len(ratings)):
                 if i != 0:
                     prev_rating = ratings[i - 1]
-                    prev_candie = candies[i - 1] or 1
+                    prev_candy = candies[i - 1]
                 if i != len(ratings) - 1:
                     next_rating = ratings[i + 1]
-                    next_candie = candies[i + 1] or 1
-                if not candies[i]:
+                    next_candy = candies[i + 1]
+                if not candies[i] and (prev_candy or next_candy):
                     if ratings[i] <= prev_rating and ratings[i] <= next_rating:
                         candies[i] = 1
                     elif ratings[i] > prev_rating and ratings[i] <= next_rating:
-                        candies[i] = prev_candie + 1
+                        candies[i] = prev_candy + 1
                     elif ratings[i] <= prev_rating and ratings[i] > next_rating:
-                        candies[i] = next_candie + 1
+                        candies[i] = next_candy + 1
                     elif ratings[i] > prev_rating and ratings[i] > next_rating:
-                        candies[i] = max(next_candie, prev_candie) + 1
+                        candies[i] = max(next_candy or 1, prev_candy or 1) + 1
         print(candies)
         return sum(candies)
