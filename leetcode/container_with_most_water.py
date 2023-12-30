2d271c92569a1529ef586bac0003996bc3da8444
Randall Knudsen <simonjknudsen@gmail.com>
2023-12-31 09:17:12 +1300

    WIP

--- leetcode/container_with_most_water.py
+++ leetcode/container_with_most_water.py
@@ -5,11 +5,30 @@ class Solution:
         if not height or len(height) == 1:
             return 0
         max_area = 0
-        for i in range(len(height)):
-            for j in range(len(height)):
-                x = abs(i-j)
-                y = min(height[i],height[j])
-                if x * y > max_area:
-                    max_area = x * y
+        #for i in range(len(height)):
+        #    for j in range(len(height)):
+        #        x = abs(i-j)
+        #        y = min(height[i],height[j])
+        #        if x * y > max_area:
+        #            max_area = x * y
+
+        #height_idx = sorted([(i, height[i]) for i in range(len(height))], key = lambda x, y : y)
+        height_idx = sorted([(i, height[i]) for i in range(len(height))], key = lambda x: x[1])
+        print(height_idx)
+
+        for i in range(len(height_idx)):
+            x = height_idx[i]
+            index = x[0]
+            area = max([abs(index - j[0]) for j in height_idx[i::]]) * x[1]
+            print(f"x={x} area={area}")
+            if area > max_area:
+                max_area = area
+
+        #for i in range(len(height)):
+        #    dists = [abs(x - i) for x in range(len(height))]
+        #    heights = [min(height[i],height[x]) for x in range(len(height))]
+        #    areas = [dists[x] * heights[x] for x in range(len(height))]
+        #    if max(areas) > max_area:
+        #        max_area = max(areas)
         return max_area
 
--- test/container_with_most_water_test.py
+++ test/container_with_most_water_test.py
@@ -5,6 +5,7 @@ class MyTestCase(unittest.TestCase):
     def test_something(self):
         s = container_with_most_water.Solution()
         self.assertEqual(49, s.maxArea([1,8,6,2,5,4,8,3,7]))
+        self.assertEqual(1, s.maxArea([1,1]))
 
 if __name__ == '__main__':
     unittest.main()
