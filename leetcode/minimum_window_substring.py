922e59257ec9af91b46e75d882af46893d06682c
Randall Knudsen <simonjknudsen@gmail.com>
2024-01-02 09:46:46 +1300

    minimum_window_substring

--- leetcode/minimum_window_substring.py
+++ leetcode/minimum_window_substring.py
@@ -1,3 +1,41 @@
 class Solution:
+
+    def create_holder(self, s):
+        return {c:-1 for c in s}
+
+    def is_complete(self, h, t):
+        return sorted(t) == sorted("".join(h["collected"]))
+
     def minWindow(self, s: str, t: str) -> str:
+        #chars = {}
+        in_progress = []
+        best = None
+        #for i in range(len(s)):
+        #    c = s[i]
+        #    if chars[c]:
+        #        chars[c].append(i)
+        #    else:
+        #        chars[c] = [i]
+        for i in range(len(s)):
+            print(f"best={best} in_progress={in_progress}")
+            c = s[i]
+            if c in t:
+                in_progress2 = in_progress.copy()
+                for p in in_progress:
+                    if p["chars"].count(c) > 0:
+                        p["chars"].remove(c)
+                    if len(p["chars"]) == 0:
+                        p["end"] = i
+                        if not best:
+                            best = p
+                        elif p["end"] - p["start"] < best["end"] - best["start"]:
+                            best = p
+                        in_progress2.remove(p)
+                new = {"start": i, "chars": list(t)}
+                new["chars"].remove(c)
+                in_progress2.append(new)
+                in_progress = in_progress2
+        if best:
+            return s[best["start"]:best["end"] + 1]
         return ""
+
--- test/minimum_window_substring_test.py
+++ test/minimum_window_substring_test.py
@@ -1,13 +1,15 @@
 import unittest
 from leetcode.minimum_window_substring import Solution
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
+class TestSolution(unittest.TestCase):
+    def test_min_window(self):
         s = Solution()
         print(s.minWindow("ADOBECODEBANC", "ABC"))
         # "ADOBECODEBANC", t = "ABC"
-        #self.assertEqual(True, False)  # add assertion here
 
+        self.assertEqual("BANC", s.minWindow("ADOBECODEBANC", "ABC"))
+        self.assertEqual("", s.minWindow("a", "aa"))
+        self.assertEqual("a", s.minWindow("a", "a"))
 
 if __name__ == '__main__':
     unittest.main()
