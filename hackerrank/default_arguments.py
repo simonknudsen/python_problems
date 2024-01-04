6b1e667682d859df11e85c8e423ee5b58bc72dc2
Randall Knudsen <simonjknudsen@gmail.com>
2024-01-05 06:43:45 +1300

    default arguments

--- hackerrank/default_arguments.py
+++ hackerrank/default_arguments.py
@@ -17,16 +17,25 @@ class OddStream(object):
         return to_return
 
 def print_from_stream(n, stream=EvenStream()):
+    result = []
+    #print(stream)
+    if type(stream) == EvenStream:
+        stream = EvenStream()
     for _ in range(n):
-        print(stream.get_next())
+        result.append(stream.get_next())
+    return result
 
-with open("../data/default_arguments_input.txt") as f_i:
-    input = [x.strip() for x in f_i.readlines()]
-    queries = int(input[0])
+def process(input):
+    #queries = int(input[0])
+    result = []
     for i in input[1::]:
+        #print(i)
         stream_name, n = i.split()
         n = int(n)
         if stream_name == "even":
-            print_from_stream(n)
+            #print("EVEN")
+            result += print_from_stream(n)
         else:
-            print_from_stream(n, OddStream())
+            #print(f"ODD {print_from_stream(n, OddStream())}")
+            result += print_from_stream(n, OddStream())
+    return result
--- test/default_arguments_test.py
+++ test/default_arguments_test.py
@@ -1,11 +1,26 @@
 import unittest
-
+from hackerrank import default_arguments
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+    def test_hackerrank_provided_answers(self):
+        #self.assertEqual(True, False)  # add assertion here
         with open("../data/default_arguments_input.txt") as f_i:
             input = [x.strip() for x in f_i.readlines()]
+            print(input)
+            result = default_arguments.process(input)
+            result = [str(x) for x in result]
+            with open("../data/default_arguments_answers.txt") as f_a:
+                ans = [x.strip() for x in f_a.readlines()]
+                print(result == ans)
+                print(f"result={len(result)} ans={len(ans)}")
+                for i in range(max(len(ans), len(ans))):
+                    a = None
+                    r = None
+                    if i < len(ans):
+                        a = ans[i]
+                    if i < len(result):
+                        r = result[i]
+                    print(f"result={r} ans={a}")
 
 if __name__ == '__main__':
     unittest.main()
